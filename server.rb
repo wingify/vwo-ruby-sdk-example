547161cd50d5db560b74f2debaef8f78ef4648fb
Abbas-khaliq <abbas.khaliq@gmail.com>
2021-07-08 15:13:35 +0530

    feat(example): webhooks, event-batching, and integrations support

--- server.rb
+++ server.rb
@@ -5,7 +5,6 @@ require "json"
 require 'time'
 require 'erb'
 require_relative 'user_storage'
-require_relative 'constants'
 config = JSON.load(File.open(File.join(File.dirname(__FILE__), 'config/vwo.json')))
 
 USERS = %w(Ashley Bill Chris Dominic Emma Faizan Gimmy Harry Ian John King Lisa Mona Nina Olivia Pete Queen Robert Sarah Tierra Una Varun Will Xin You Zeba)
@@ -29,8 +28,36 @@ class VWO
   end
 end
 
-$vwo_client_instance = VWO.new(config['account_id'], config['sdk_key'], nil, nil, false)
-$vwo_client_instance_user_storage = VWO.new(config['account_id'], config['sdk_key'], nil, VWO::UserStorage.new, false)
+def flush_callback(error, events)
+  puts "flush_callback method"
+  puts events
+end
+
+def integrations_callback(properties)
+  puts "properties from callback"
+  puts properties
+end
+
+$vwo_client_instance = VWO.new(
+  config['account_id'],
+  config['sdk_key'],
+  nil,
+  nil, #VWO::UserStorage.new
+  false,
+  nil,
+  {
+    # batch_events: {
+    #   events_per_request: 3,
+    #   request_time_interval: 20,
+    #   flushCallback: method(:flush_callback)
+    #   },
+    # integrations: {
+    #   callback: method(:integrations_callback)
+    # },
+    # should_track_returning_user: false,
+    # goal_type_to_track: "REVENUE"
+  }
+)
 
 # Logging After Starting App
 def start_logger
@@ -105,14 +132,12 @@ get '/user_storage' do
   user_id = params['userId'] || USERS.sample
   revenue_value = params['revenue'].to_i
   ab_campaign_goal_identifier = params['ab_campaign_goal_identifier']
-
-  variation_name = $vwo_client_instance_user_storage.activate(ab_campaign_key, user_id)
-  $vwo_client_instance_user_storage.track(ab_campaign_key, user_id, ab_campaign_goal_identifier, revenue_value)
-
+  variation_name = $vwo_client_instance.activate(ab_campaign_key, user_id)
+  $vwo_client_instance.track(ab_campaign_key, user_id, ab_campaign_goal_identifier, revenue_value)
   erb :template, locals: {
     part_of_campaign: variation_name.nil? ? 'No' : 'Yes',
     variation_name: variation_name,
-    settings_file: $vwo_client_instance_user_storage.get_settings,
+    settings_file: $vwo_client_instance.get_settings,
     ab_campaign_key: ab_campaign_key,
     ab_campaign_goal_identifier: ab_campaign_goal_identifier,
     user_id: user_id
@@ -121,75 +146,96 @@ end
 
 get '/ab' do
   user_id = params['userId'] || USERS.sample
-  variation_name = $vwo_client_instance_user_storage.activate(AbCampaignData['campaign_key'], user_id, { custom_variables: AbCampaignData['custom_variables'] })
+  variation_name = $vwo_client_instance_user_storage.activate(config["ab_campaign_key"], user_id, { custom_variables: config["ab_custom_variables"] })
   is_part_of_campaign = !variation_name.nil?
-  $vwo_client_instance_user_storage.track(AbCampaignData['campaign_key'], user_id, AbCampaignData['campaign_goal_identifier'], { revenue_value: AbCampaignData['revenue_value'] })
-  
+  $vwo_client_instance_user_storage.track(config["ab_campaign_key"], user_id, config["ab_campaign_goal_identifier"], { revenue_value: config["ab_custom_variables"] })
   erb :ab, locals: {
     user_id: user_id,
     campaign_type: "Visual-AB",
     is_part_of_campaign: is_part_of_campaign,
     variation_name: variation_name,
-    ab_campaign_key: AbCampaignData['campaign_key'],
-    ab_campaign_goal_identifier: AbCampaignData['campaign_goal_identifier'],
-    custom_variables: JSON.generate(AbCampaignData['custom_variables']),
-    settings_file: $vwo_client_instance_user_storage.get_settings
+    ab_campaign_key: config["ab_campaign_key"],
+    ab_campaign_goal_identifier: config["ab_campaign_goal_identifier"],
+    custom_variables: JSON.generate(config["ab_custom_variables"]),
+    settings_file: $vwo_client_instance.get_settings
   }
 end
 
+post '/webhook' do
+  content_type :json
+  if config['webhook_auth_key'] and request.env['HTTP_X_VWO_AUTH']
+    if config['webhook_auth_key'] != request.env['HTTP_X_VWO_AUTH']
+      puts('webhook api authentication failed')
+      return {'status': 'failed', 'message': 'webhook api authentication failed'}.to_json
+    else
+      puts('Webhook api authentication successful')
+    end
+  else
+    puts('Skipping authentication as missing webhook authentication key')
+  end
+  $vwo_client_instance.get_and_update_settings_file
+  return {'status': 'success', 'message': 'settings updated successfully'}.to_json
+end
+
 get '/feature-rollout' do
-  user_id = params['userId'] || FeatureRolloutData['user_id'] || USERS.sample
-  is_user_part_of_feature_rollout_campaign = $vwo_client_instance.feature_enabled?(FeatureRolloutData['campaign_key'], user_id, { custom_variables: FeatureRolloutData['custom_variables'] })
-  
+  user_id = params['userId'] || USERS.sample
+  is_user_part_of_feature_rollout_campaign = $vwo_client_instance.feature_enabled?(config["feature_rollout_campaign_key"], user_id, { custom_variables: config['feature_rollout_custom_variables'] })
   erb :feature_rollout, locals: {
     user_id: user_id,
     campaign_type: 'Feature-rollout',
     is_user_part_of_feature_rollout_campaign: is_user_part_of_feature_rollout_campaign,
-    feature_rollout_campaign_key: FeatureRolloutData['campaign_key'],
-    custom_variables: JSON.generate(FeatureRolloutData['custom_variables']),
+    feature_rollout_campaign_key: config["feature_rollout_campaign_key"],
+    custom_variables: JSON.generate(config['feature_rollout_custom_variables']),
     settings_file: $vwo_client_instance.get_settings
   }
 end
 
 get '/feature-test' do
-  user_id = params['userId'] || FeatureTestData['user_id'] || USERS.sample
-  is_user_part_of_feature_campaign = $vwo_client_instance.feature_enabled?(FeatureTestData['campaign_key'], user_id)
+  user_id = params['userId'] || USERS.sample
+  is_user_part_of_feature_campaign = $vwo_client_instance.feature_enabled?(config["feature_test_campaign_key"], user_id)
   $vwo_client_instance.track(
-    FeatureTestData['campaign_key'],
+    config["feature_test_campaign_key"],
     user_id,
-    FeatureTestData['campaign_goal_identifier'],
-    revenue_value=FeatureTestData['revenue_value'],
-    custom_variables=FeatureTestData['custom_variables']
+    config["feature_test_goal_identifier"],
+    revenue_value=config['feature_test_revenue_value'],
+    custom_variables=config['feature_test_custom_variables']
   )
-  string_variable = $vwo_client_instance.get_feature_variable_value(FeatureTestData['campaign_key'], FeatureTestData['string_variable_key'], user_id, { custom_variables: FeatureTestData['custom_variables'] })
-  integer_variable = $vwo_client_instance.get_feature_variable_value(FeatureTestData['campaign_key'], FeatureTestData['integer_variable_key'], user_id, { custom_variables: FeatureTestData['custom_variables'] })
-  boolean_variable = $vwo_client_instance.get_feature_variable_value(FeatureTestData['campaign_key'], FeatureTestData['boolean_variable_key'], user_id, { custom_variables: FeatureTestData['custom_variables'] })
-  double_variable = $vwo_client_instance.get_feature_variable_value(FeatureTestData['campaign_key'], FeatureTestData['double_variable_key'], user_id, { custom_variables: FeatureTestData['custom_variables'] })
-  
+
+  string_variable = $vwo_client_instance.get_feature_variable_value(config["feature_test_campaign_key"], config['string_variable_key'], user_id, { custom_variables: config['feature_test_custom_variables'] })
+  integer_variable = $vwo_client_instance.get_feature_variable_value(config["feature_test_campaign_key"], config['integer_variable_key'], user_id, { custom_variables: config['feature_test_custom_variables'] })
+  boolean_variable = $vwo_client_instance.get_feature_variable_value(config["feature_test_campaign_key"], config['boolean_variable_key'], user_id, { custom_variables: config['feature_test_custom_variables'] })
+  double_variable = $vwo_client_instance.get_feature_variable_value(config["feature_test_campaign_key"], config['double_variable_key'], user_id, { custom_variables: config['feature_test_custom_variables'] })
+
   erb :feature_test, locals: {
     user_id: user_id,
     campaign_type: "Feature-test",
     is_user_part_of_feature_campaign: is_user_part_of_feature_campaign,
-    feature_campaign_key: FeatureTestData['campaign_key'],
-    feature_campaign_goal_identifier:  FeatureTestData['campaign_goal_identifier'],
+    feature_campaign_key: config['feature_test_campaign_key'],
+    feature_campaign_goal_identifier: config["feature_test_goal_identifier"],
     string_variable: string_variable,
     integer_variable: integer_variable,
     boolean_variable: boolean_variable,
     double_variable: double_variable,
-    custom_variables: JSON.generate(FeatureRolloutData['custom_variables']),
+    custom_variables: JSON.generate(config['feature_test_custom_variables']),
     settings_file: $vwo_client_instance.get_settings
   }
 end
 
 get '/push' do
   user_id = params['userId'] || USERS.sample
-  result = $vwo_client_instance.push(PushData['tag_key'], PushData['tag_value'], user_id)
-  
+  result = $vwo_client_instance.push(config['tag_key'], config['tag_value'], user_id)
   erb :push, locals: {
     user_id: user_id,
-    tag_key: PushData['tag_key'],
-    tag_value: PushData['tag_value'],
+    tag_key: config['tag_key'],
+    tag_value: config['tag_value'],
     result: result,
     settings_file: $vwo_client_instance.get_settings
+  }
+end
+
+get '/flush-events' do
+  result = $vwo_client_instance.flush_events
+  erb :flush_events, locals: {
+    result: result
   }
 end
